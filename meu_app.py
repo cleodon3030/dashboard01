fcceb2c64cafa0a9fd35a426eb4eb026d63089c6
Cleodon <cleodonfilho@gmail.com>
2024-01-04 18:50:39 -0300

    0.0.4 (path relativo)

--- meu_app.py
+++ meu_app.py
@@ -1,14 +1,30 @@
 import streamlit as st 
 import pandas as pd
+from pathlib import Path 
+caminho_projeto = Path()
+caminho_arquivo = Path(__file__)
+diretorio_arquivo = caminho_arquivo.parent
+arquivo = diretorio_arquivo / 'resultados.csv'
+
+#arquivo = str(Path())+'resultados.csv'
+
 #----------Configurando a página  
 st.set_page_config(page_title='Dashboard01')
-arquivo = 'c:/treinamento/streamlit/proj01/venv/resultados.csv'
+
+#arquivo = 'c:/treinamento/streamlit/proj01/venv/resultados.csv'
+#arquivo = '.\resultados.csv'
 with st.container():
-    st.write('Meu primeiro site com stremlit 0.0.3.')
+    st.write('Meu primeiro site com stremlit 0.0.6.')
     st.title('Título')
     st.subheader('Subheader')
     st.write('Informações sobre os contratos fechados pela empresa Cleo S/A')
     st.write('Quer aprender Pyton? [Clique Aqui](https://www.hashtagtreinamentos.com/curso-python)')
+    st.write(f'caminho_projeto :  {caminho_projeto}')
+    st.write(f'caminho_arquivo :  {caminho_arquivo}')
+    st.write(f'diretorio_arquivo :  {diretorio_arquivo}')
+    st.write(f'arquivo :  {arquivo}')
+
+
 with st.container():
    st.write('---')
 #--decorator está associado a uma função logo abaixo dele   
